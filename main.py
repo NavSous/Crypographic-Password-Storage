e6a37d8c81d16883338846c8745152804452241c
NavSous <navhs360@gmail.com>
2021-12-06 14:09:34 -0800

    Added Monetary Transfer System

--- main.py
+++ main.py
@@ -11,12 +11,13 @@ salt = "stoprainbowtables"
 
 #Class for creating a user object (Create a user using: User(id, username, password))
 class User:
-    def __init__(self, id, username, password):
+    #Add balance to user objects
+    def __init__(self, id, username, password, balance):
         self.id = id
         self.username = username
         #Ensure the password is safe (It is unsafe it is all one case or it lacks any special characters)
         if len(password) < 8 or not any(a in special_characters for a in password) or password.upper() == password or password.lower() == password:
-            print("That password is not storng enough")
+            print("That password is not strong enough")
             return None
         #Salt the password and then hash it using sha256
         salted = password + salt
@@ -30,10 +31,13 @@ class User:
             x = row
         if x!=None:
             print("That username or ID is already in use")
+        #ADD ENCRYPTION FOR BALANCE
         else:
             #Insert data into the SQLite database
-            cur.execute("INSERT INTO users (id, username, password) VALUES (?, ?, ?)", (self.id, self.username, pwd))
+            cur.execute("INSERT INTO users (id, username, password, balance) VALUES (?, ?, ?, ?)", (self.id, self.username, pwd, balance))
             con.commit()
+
+#User(11, "Nav2", "What/isuP", 100)
 
 #Function for authenticating the user
 def login(uname, pword):
@@ -56,6 +60,25 @@ def login(uname, pword):
     if Auth:
         print("You have been successfully authenticated")
     return Auth, AuthId
+#Function for monetary transfers
+def transfer(cur_user, amount, recipient_usrname):
+    #MAKE SURE TO DECRYPT cur_user[3] using plaintext PWD
+    #Get recipient user object
+    for row in cur.execute('''SELECT * FROM users WHERE username = ?''', (recipient_usrname,)):
+        x = row
+    #If the user has enough funds
+    if amount <= cur_user[3]:
+        #Subtract from current user's balance
+        cur_balance = cur_user[3] - amount
+        #Add to recipient's balance
+        recip_balance = x[3] + amount
+    #If the user does not have enough funds
+    else:
+        cur_balance = cur_user[3]
+        recip_balance = x[3]
+        print("You don't have enough funds for this transaction")
+    #RENCRYPT BALANCE BEFORE RETURN
+    return(cur_balance, recip_balance, x[0])
 
 #Promt the user to enter their information
 usrname = input("Enter you username: ")
@@ -69,10 +92,18 @@ if auth_result[0]:
         x = row
     #Print a success statement
     print("You are know logged in to the account with these properties: ", x)
-    #Allow the user to create a post to the database
-    ip = input("Enter what you want your post to be: ")
-    cur.execute("INSERT INTO posts (user_id, user_username, comment_text) VALUES (?, ?, ?)", (x[0], x[1], ip))
+    #Allow the user to make a transfer
+    
+    #ADD PASSWORD AUTH BEFORE EXECUTING STUFF
+    y = input("To make a transfer, enter the amount you want to transfer and the username of the person you want to transfer to, seperated by a space: ")
+    y1 = y.split(" ")
+    z = transfer(x, int(y1[1]), y1[0])
+
+    #Save result of transfer to the database
+    cur.execute("UPDATE users SET balance = ? WHERE id = ?", (z[0], x[0]))
+    cur.execute("UPDATE users SET balance = ? WHERE id = ?", (z[1], z[2]))
     con.commit()
+
 #If the authentication failed
 else:
     print("Incorrect username or password")
